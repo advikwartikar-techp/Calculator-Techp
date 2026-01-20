8b692e69beaf0e5a79d6f3079268f439512876af
Advik Wartikar <advik.wartikar@techprescient.com>
2026-01-20 16:19:57 +0530

    Client Commit

---
+++ Client.java
@@ -0,0 +1,7 @@
+public class Client {
+    public static void main(String[] args) {
+        Calculation calc = new Calculation();
+        int result = calc.calculate(10, 5);
+        System.out.println("Result = " + result);
+    }
+}
