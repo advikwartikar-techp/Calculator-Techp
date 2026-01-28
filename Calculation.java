680b51c90f505aaf103b52f398914f2b99768246
Advik Wartikar <advik.wartikar@techprescient.com>
2026-01-28 10:38:22 +0530

    Add Java Documentation to Multiply

--- Calculation.java
+++ Calculation.java
@@ -1,5 +1,10 @@
-// Multiplies two numbers
-
-public int multiply(int a, int b) {
+/**
+ * Multiplies two integers and returns their product.
+ *
+ * @param a the first integer value
+ * @param b the second integer value
+ * @return the product of {@code a} and {@code b}
+ */
+public static int multiply(int a, int b) {
     return a * b;
 }
