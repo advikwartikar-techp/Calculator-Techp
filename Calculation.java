a94cb7326e6b6fce3f626a94cf68523e5d2b2b6a
Advik Wartikar <advik.wartikar@techprescient.com>
2026-01-30 12:48:15 +0530

    Add absolute value method

--- Calculation.java
+++ Calculation.java
@@ -1,6 +1,3 @@
-public static int multiply(int a, int b) {
-    if (a != 0 && b != 0 && a > Integer.MAX_VALUE / b) {
-        throw new ArithmeticException("Integer overflow");
-    }
-    return a * b;
+public int abs(int a) {
+    return a < 0 ? -a : a;
 }
